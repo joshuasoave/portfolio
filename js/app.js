a3a06079b5995bd4744ad20a8f6f69c11b908e48
Joshua Soave <joshua.soave@gmail.comgit config --global user.name JoshuaSoave>
2020-07-21 09:41:53 -0400

    updated styling for pokemon card

--- js/app.js
+++ js/app.js
@@ -4,3 +4,16 @@ $(document).ready(function(){
  });
 
 console.log('hi');
+
+//Email
+
+var template_params = {
+   "reply_to": "reply_to_value",
+   "from_name": "from_name_value",
+   "to_name": "to_name_value",
+   "message_html": "message_html_value"
+}
+
+var service_id = "default_service";
+var template_id = "template_FhDN1WGR";
+emailjs.send(service_id, template_id, template_params);
